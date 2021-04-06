067443b1bd1120fb855b5f319efa585f65161817
Andrzej <andrzej.rakowski79@gmail.com>
2021-04-06 19:57:50 +0200

    Slider added

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,3 +1,47 @@
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('#next');
+const prevBtn = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for next slide
+    if(current.nextElementSibling) {
+        //Add current to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        //Add current to start
+        slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+const prevSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for prev slide
+    if(current.previousElementSibling) {
+        //Add current to prev sibling
+        current.previousElementSibling.classList.add('current');
+    } else {
+        //Add current to last
+        slides[slides.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+// Button events
+
+nextBtn.addEventListener('click', (e) => nextSlide());
+prevBtn.addEventListener('click', (e) => prevSlide());
+
 // // ====== MENU SHOW AND HIDE ======
 
 // const navMenu = document.querySelector('.nav'),
@@ -40,7 +84,6 @@
 // ACCORDION
 
 const accordionTitles = document.querySelectorAll('.accordion__title');
-console.log(accordionTitles);
 
 accordionTitles.forEach((accordionTitle) => {
     accordionTitle.addEventListener('click', () => {
